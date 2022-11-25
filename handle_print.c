6c3a495f4d09243b8b2b56033c384b1d6247ec1c
Ayuniya8100 <Ayujimma.8100@gmail.com>
2022-11-25 08:44:57 +0000

    my printf

--- handle_print.c
+++ handle_print.c
@@ -16,19 +16,19 @@ int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, 
-		{'s', print_string}, 
+		{'c', print_char},
+		{'s', print_string},
 		{'%', print_percent},
-		{'i', print_int}, 
-		{'d', print_int}, 
+		{'i', print_int},
+		{'d', print_int},
 		{'b', print_binary},
-		{'u', print_unsigned}, 
-		{'o', print_octal}, 
+		{'u', print_unsigned},
+		{'o', print_octal},
 		{'x', print_hexadecimal},
-		{'X', print_hexa_upper}, 
-		{'p', print_pointer}, 
+		{'X', print_hexa_upper},
+		{'p', print_pointer},
 		{'S', print_non_printable},
-		{'r', print_reverse}, 
+		{'r', print_reverse},
 		{'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
